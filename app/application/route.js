2b90ebaf4b38737991c68e2d8e6a65711476318f
Gabriel Smolana <smolana.gabriel@gmail.com>
2022-05-07 15:24:51 +0200

    Added post page

--- app/application/route.js
+++ app/application/route.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
+// post > show > temp wrzucic do komponentu <Post::Card @posts={{this.model}}>
+
 export default class ApplicationRoute extends Route {
   @service store;
 
@@ -10,7 +12,8 @@ export default class ApplicationRoute extends Route {
       username: 'admin',
       password: 'admin123',
       email: 'admin@admin.com',
-      photoURL: 'https://funnycat.pl/wp-content/uploads/2020/02/koty-i-pude%C5%82ka-256x256.png',
+      photoURL:
+        'https://funnycat.pl/wp-content/uploads/2020/02/koty-i-pude%C5%82ka-256x256.png',
       isAdmin: true,
     };
 
@@ -19,7 +22,8 @@ export default class ApplicationRoute extends Route {
       username: 'user',
       password: 'user123',
       email: 'user@user.com',
-      photoURL: 'https://a.allegroimg.com/s400/118184/e23b5cb54e7596e117f51a3eab02/MAGNES-NA-LODOWKE-PIES'
+      photoURL:
+        'https://a.allegroimg.com/s400/118184/e23b5cb54e7596e117f51a3eab02/MAGNES-NA-LODOWKE-PIES',
     };
 
     const user1Model = this.store.createRecord('user', user1);
@@ -29,18 +33,21 @@ export default class ApplicationRoute extends Route {
     await user2Model.save();
 
     const post1 = {
+      id: 1,
       title: 'Tytuł testowy 1',
       body: 'Zawartość testowa 1',
       owner: user1Model,
     };
 
     const post2 = {
+      id: 2,
       title: 'Tytuł testowy 2',
       body: 'Zawartość testowa 2',
       owner: user1Model,
     };
 
     const post3 = {
+      id: 3,
       title: 'Tytuł testowy 3',
       body: 'Zawartość testowa 3',
       owner: user2Model,
