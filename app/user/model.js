0e6f7f553d7fcfc9b752502908cd2a3ece87d627
Gabriel Smolana <smolana.gabriel@gmail.com>
2022-05-07 12:10:56 +0200

    Added user model, serializer and adapter

--- app/user/model.js
+++ app/user/model.js
@@ -1,11 +1,9 @@
-import Model, { attr }  from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class UserModel extends Model {
-    @attr id;
-    @attr username;
-    @attr password;
-    @attr email;
-    @attr isDeleted;
-    @attr isAdmin;
-        
+  @attr('string') username;
+  @attr('string') password;
+  @attr('string') email;
+  @attr('boolean', { defaultValue: false }) isDeleted;
+  @attr('boolean', { defaultValue: false }) isAdmin;
 }
