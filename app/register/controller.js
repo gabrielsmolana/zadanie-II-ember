f4e1cfb202a82e183f67307273a4840b4755b9df
Gabriel Smolana <smolana.gabriel@gmail.com>
2022-05-14 13:45:38 +0200

    handle user register

--- app/register/controller.js
+++ app/register/controller.js
@@ -4,29 +4,35 @@ import { inject as service} from '@ember/service'
 
 export default class RegisterController extends Controller{
     @service store;
+    @service router
 
     @action
-    onUsernameChange(){
+    onUsernameChange({target : { value }}){
+        this.model.username = value;
+    }
+
+    @action
+    onEmailChange({target : { value }}){
+        this.model.email = value;
 
     }
 
     @action
-    onEmailChange(){
-
+    onPasswordChange({target : { value }}){
+        this.model.password = value;
     }
 
     @action
-    onPasswordChange(){
-
-    }
-
-    @action
-    onPhotoURLChange(){
-
+    onPhotoURLChange({target : { value }}){
+        this.model.photoURL = value;
     }
 
     @action
     async onSubmit(event){
         event.preventDefault();
+
+        await this.model.save()
+
+        this.router.transitionTo('login')
     }
 }
