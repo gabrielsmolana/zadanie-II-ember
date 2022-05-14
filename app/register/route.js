6362f7eb16c5248656c8f5128ade78d453e5ada9
Gabriel Smolana <smolana.gabriel@gmail.com>
2022-05-14 13:49:02 +0200

    register - check if logged in redirect

--- app/register/route.js
+++ app/register/route.js
@@ -1,8 +1,21 @@
 import Route from '@ember/routing/route';
+import { storageFor } from 'ember-local-storage' 
 import { inject as service } from '@ember/service'
 
 export default class RegisterRoute extends Route {
     @service store
+    @service router
+
+    @storageFor('logged-as') loggedAs
+
+    beforeModel(){
+        const isLoggedIn = this.loggedAs.get('id') == null ? false : true
+        
+        if(isLoggedIn){
+            this.router.transitionTo('home')
+            return
+        }
+    }
 
     model(){
         return this.store.createRecord('user')
