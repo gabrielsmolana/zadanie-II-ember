d3ac6658bb825a9d07e8b4b02fb8967cc9b26b99
Gabriel Smolana <smolana.gabriel@gmail.com>
2022-06-11 11:25:54 +0200

    add create day to post

--- app/post/model.js
+++ app/post/model.js
@@ -4,6 +4,17 @@ export default class PostModel extends Model {
   @attr('string') title;
   @attr('string') body;
   @attr('boolean', { defaultValue: false }) isDeleted;
+  @attr('date', {defaultValue: () => new Date()}) createdAt;
   @belongsTo('user', { autoSave: true }) owner;
   @hasMany('like') likes;
+
+  get fullDate(){
+    const date = new Date(this.createdAt);
+
+    const day = date.getDate();
+    const month = date.getMonth()+1;
+    const year = date.getFullYear();
+
+    return `${day}.${month}.${year}`;
+  }
 }
