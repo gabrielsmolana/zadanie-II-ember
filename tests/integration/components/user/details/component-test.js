c0abfc98164ece352684e209c22677af9fed1d16
Gabriel Smolana <smolana.gabriel@gmail.com>
2022-07-02 16:17:08 +0200

    add user details test

--- tests/integration/components/user/details/component-test.js
+++ tests/integration/components/user/details/component-test.js
@@ -2,25 +2,17 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import { faker } from '@faker-js/faker';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | user/details', function (hooks) {
   setupRenderingTest(hooks);
-
-  const user = {
-    id: 1,
-    username: faker.name.firstName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-    photoURL: faker.image.avatar(),
-  };
+  setupMirage(hooks);
 
   test('it renders', async function (assert) {
+    const user = this.server.create('user');
     this.set('user', user);
 
     await render(hbs`<User::Details @user={{this.user}}/>`);
-
-    await this.pauseTest();
 
     assert.dom('[data-test-username]').hasText(user.username);
     assert.dom('[data-test-id]').hasText(user.id.toString());
